b4054fd2a5c6a70d3d3539817abf9ca8694178a2
LynetteA <36066063+LynetteA@users.noreply.github.com>
2018-07-03 12:11:24 +0300

    Add files via upload

---
+++ space_and_service-java-lynette.java
@@ -0,0 +1,13 @@
+package com.example.patricia.mikolo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class space_and_service extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_space_and_service);
+    }
+}
